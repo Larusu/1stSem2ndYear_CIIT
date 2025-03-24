4b494c660f7100621e9855368f5d90f6b96d1381
LD <ldaneliee33@gmail.com>
2025-03-24 18:17:44 +0800

    Organizing CIIT Files

---
+++ Conversion.java
@@ -0,0 +1,28 @@
+// Create a new Java project and name it with Week3[your last name][your first name] (e.g. Week3JuanCruz). 
+// Get an integer input from the user, select the type of measurement then select from a dropdown list the conversion type.
+// Unit of conversion for length
+
+// meter [m]
+// kilometer [km]
+// centimeter [cm]
+// millimeter [mm]
+// mile [mi, mi(Int)]
+// yard [yd]
+// foot [ft]
+// inch [in]
+//   example:
+// Enter value :1
+
+// Select unit of measurement : inches
+
+// Select conversion unit : centimeter
+
+// Conversion Result is : 1 inches = 2.54 centimeter
+//import java.util.Scanner;
+public class Conversion{
+    public static void main(String[] args) {
+        //Scanner scan = new Scanner(System.in);
+        System.out.println("======Conversion for Length======");
+
+    }
+}
